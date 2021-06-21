5dd8ce584821d85f2022d9e853a7fc5e93523b10
Alessio Ricci <ale.ricci@hispeed.ch>
2021-06-21 21:38:10 +0200

    Day1

--- todo-angular/src/app/app.component.spec.ts
+++ todo-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ToDo'`, () => {
+  it(`should have as title 'todo-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ToDo');
+    expect(app.title).toEqual('todo-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ToDo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('todo-angular app is running!');
   });
 });
